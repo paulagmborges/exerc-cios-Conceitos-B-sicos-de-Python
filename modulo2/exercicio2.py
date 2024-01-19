7cf857eb2eba037e078f04b8d959c12df8499e7a
paula borges <paulabzambeli@gmail.com>
2024-01-19 17:06:03 -0300

    exercico mod2 ate o 3 finalizado

---
+++ modulo2/exercicio2.py
@@ -0,0 +1,17 @@
+#2.Faça um Programa que pergunte em que turno você estuda.
+#Peça para digitar M-matutino ou V-Vespertino ou N-Noturno.
+#Imprima a mensagem"BomDia!","BoaTarde!"ou"BoaNoite!"ou"ValorInválido!",conforme o caso.
+
+
+turno = input("Em qual turno você estuda? Digite M para Matutino, V para Vespertino ou N para Noturno: ")
+
+if turno.upper() == 'M':
+    mensagem = "Bom Dia!"
+elif turno.upper() == 'V':
+    mensagem = "Boa Tarde!"
+elif turno.upper() == 'N':
+    mensagem = "Boa Noite!"
+else:
+    mensagem = "Valor Inválido!"
+
+print(mensagem)
